8a880f42aed07c595033c4dd8311617176422e72
hossein <143866200+hos3sein@users.noreply.github.com>
2023-10-23 21:10:55 +0330

    Add files via upload

---
+++ turtle.py
@@ -0,0 +1,25 @@
+from turtle import *
+
+# t.getscreen()
+
+# t.pensize(10)
+# t.pencolor('blue')
+
+# t.right(90)
+# t.forward(100)
+# t.left(90)
+# t.backward(100)
+
+# t.circle(90)
+
+speed(0)
+bgcolor('black')
+color('cyan')
+
+for i in range(500):
+	fd(i*2/2+1)
+	lt(360/2+1.50)
+
+done()
+
+
